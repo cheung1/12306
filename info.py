325b0481c9612c0dd9d2e68a4a50ba3166e422da
Cheung <31495117+cheung1@users.noreply.github.com>
2018-01-09 12:33:06 +0800

    Create info.py

---
+++ info.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python
+# -*- encoding: utf-8 -*-
+__author__ = 'Cheung'
+
+user = '774405502@qq.com'
+pwd = 'wo123456'
+TRAIN_URL = 'https://kyfw.12306.cn/otn/leftTicket/queryZ'
+TRAIN_DATE = "2018-01-13"
+FROM_STATION = '深圳'
+TO_STATION = '西安'
+HARD_SEAT = 29
+# HARD_BED = 28
+# NO_SEAT = 26
+# SOFT_SEAT = 25
+# SOFT_BED = 23
+# FIRST_SEAT = 31
+# SECOND_SEAT = 30
+# # HEAD_SEAT = 32
